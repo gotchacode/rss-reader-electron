7e479c76d4ccd626292669bbb647ef5a25715918
Vinit Kumar <vinit.kumar@socialschools.nl>
2018-05-20 02:58:54 +0530

    add pouchdb

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'reactstrap';
+import PouchDB from 'pouchdb';
 import Sidebar from './components/Sidebar/index';
 import GetRSSFeedData from './components/List/GetRSSFeedData';
 import logo from './logo.svg';
