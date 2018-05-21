ce60788349b8bc6d3d5144a4a7aaac4628897afb
Vinit Kumar <vinit.kumar@socialschools.nl>
2018-05-21 17:39:55 +0530

    generate sidebar dynamically

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment} from 'react';
+import { AllFeed } from '../../FeedCrud/index';
+import { Route, Link } from 'react-router-dom';
+
 
 const sidebarData = [
   'Ars',
@@ -12,11 +15,31 @@ const sidebarData = [
 
 
 class Sidebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null
+    }
+  }
+
+  componentDidMount() {
+    let userDataPromise = AllFeed();
+    userDataPromise.then((data) => {
+      console.log('data', data);
+      this.setState({data: Object.values(data)})
+    });
+  }
+
   render() {
+    let sideBarContainer = [];
     return(
-      sidebarData.map((data, key) =>  (
-        <li key={key}>{data}</li>
-      ))
+      <Fragment>
+        {this.state.data && this.state.data.map((entry, key) => (
+          <li key={key}>
+            <a data-url={entry.doc.feedURL} href="#">{entry.doc.title}</a>
+          </li>
+        ))}
+      </Fragment>
     );
   }
 }
